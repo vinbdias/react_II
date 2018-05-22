795b50014ea77a121aa754ecbc0492a10bcfc2ec
Vinícius Dias <vintheman@gmail.com>
2018-05-21 23:23:31 -0300

    Commit até aula 02, atividade 01.

--- instalura/src/App.js
+++ instalura/src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/reset.min.css';
+import './css/timeline.css';
+import Header from './componentes/Header';
+import Timeline from './componentes/Timeline';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div id="root">
+            <div data-reactroot="" className="main">
+            <Header />
+            <Timeline />
+            </div> 
+        </div>
     );
   }
 }
